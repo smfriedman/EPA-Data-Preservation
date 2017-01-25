25ab047e7895b116a4e4681dbf5f1177de96cd07
Ethan Jaynes <ejaynes@wustl.edu>
2017-01-25 11:44:55 -0600

    added sleep, temp error bypass

--- AirDataFarmer.js
+++ AirDataFarmer.js
@@ -6,11 +6,18 @@ var driver = new webdriver.Builder()
     //.forBrowser('chrome')
     .forBrowser('chrome')
     .build();
-
 //.then required for the DOM to build in time
 driver.get('https://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/download_files.html');
 driver.findElements(By.partialLinkText('hourly')).then(function(links){
 	for (i = 0; i < links.length; i++){
-		links[1].click();
+		driver.sleep(500);
+		try{
+			links[i].click();
+		}finally{};
 	}
 });
+// driver.findElements(By.partialLinkText('hourly_42')).then(function(links){
+// 	for (i = 0; i < links.length; i++){
+// 		links[i].click();
+// 	}
+// });
