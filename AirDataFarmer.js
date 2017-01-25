55324c628e140f33a31be228627b5d4145fe16a2
Ethan Jaynes <ejaynes@wustl.edu>
2017-01-25 02:19:13 -0600

    first AirData scraper build, for hourly links

---
+++ AirDataFarmer.js
@@ -0,0 +1,16 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    //.forBrowser('chrome')
+    .forBrowser('chrome')
+    .build();
+
+//.then required for the DOM to build in time
+driver.get('https://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/download_files.html');
+driver.findElements(By.partialLinkText('hourly')).then(function(links){
+	for (i = 0; i < links.length; i++){
+		links[1].click();
+	}
+});
